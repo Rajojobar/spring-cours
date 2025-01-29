e1dfda1750ebb5d398b25330b64b26ebed41dba9
Raphaël BAR <raphaelbar003e@gmail.com>
2025-01-29 17:47:08 +0100

    premier commit

---
+++ src/main/java/fr/iut/blablamvc/BlablamvcApplication.java
@@ -0,0 +1,13 @@
+package fr.iut.blablamvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlablamvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlablamvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/iut/blablamvc/BlablamvcApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.iut.blablamvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlablamvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
