9f75eb15a10e18e293ddd872161e8cc775cea877
Raphaël BAR <raphaelbar003e@gmail.com>
2025-01-30 20:44:07 +0100

    Ajout du CRUD

--- src/main/java/fr/iut/blablamvc/controller/HomeController.java
+++ src/main/java/fr/iut/blablamvc/controller/HomeController.java
@@ -9,4 +9,10 @@ public class HomeController {
     public String home(){
         return "home";
     }
+
+
+    @GetMapping("/cgv")
+    public String cgv(){
+        return "cgv";
+    }
 }
---
+++ src/main/java/fr/iut/blablamvc/model/Produit.java
@@ -0,0 +1,61 @@
+package fr.iut.blablamvc.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Produit {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String designation;
+
+    private double prix;
+
+    private int quantite;
+
+    // Integration of the CRUD methods
+
+    public Produit() {
+    }
+
+    public Produit(String designation, double prix, int quantite) {
+        this.designation = designation;
+        this.prix = prix;
+        this.quantite = quantite;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    
+}
